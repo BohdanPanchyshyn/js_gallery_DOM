90e7d766151fa8911c4e33bd31885ded85378568
SageUA <panchyshyn33@gmail.com>
2025-12-09 17:33:19 +0200

    Solution 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,11 @@ nav.addEventListener('click', (even) => {
   even.preventDefault();
 
   const mainImage = document.querySelector('#largeImg');
+  const link = even.target.closest('a').href;
 
-  mainImage.src = even.target.closest('a').href;
+  if (!link) {
+    return;
+  }
+
+  mainImage.src = link;
 });
